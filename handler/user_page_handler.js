8d1926789119c7412a9d6d8beaa66976c77c9cb6
mf-dev <matchbookfoundationdev@gmail.com>
2020-08-13 11:15:08 +0900

    user profile + minor cosmetics

--- handler/user_page_handler.js
+++ handler/user_page_handler.js
@@ -8,11 +8,17 @@ module.exports = function(req, res) {
     var input = {};
     input.user_id = req.query.user_id;
 
+    // Set userId from session (if not passed in the query, assume viewing my profile)
+    if (!input.user_id) {
+        input.user_id = req.session_user.user_id;
+    }
+
     userService.loadUser(input, function(err, output) {
 
         if (err) {
-            res.render('error_page', {});
-            return;
+            return res.render('error_page', {
+                err: err
+            });
         }
 
         res.render('user_page', {
