0611a400bd5f3be38880d1cf23779202d39d69d8
mf-dev <matchbookfoundationdev@gmail.com>
2020-09-03 15:47:10 +0900

    Fix https redirect header extraction logic

--- middleware/https_redirect_middleware.js
+++ middleware/https_redirect_middleware.js
@@ -1,14 +1,35 @@
 
 module.exports = function(req, res, next) {
 
-    var headerProtocol = req.headers['X-Forwarded-Proto']; // 'http' | 'https' as populated by AWS Elastic Load Balancer
+    var headerHost = extractHostFromRequest(req);
+    var headerProtocol = extractProtocolFromRequest(req);
 
-    // Redirect https => http
-    if (headerProtocol === 'http') {
-
-        var httpsUrl = 'https://' + request.headers.host;
-        return res.redirect(301, httpsUrl);
+    // Redirect http => https
+    if (headerHost == 'matchbook.academy' && headerProtocol == 'http') {
+        return res.redirect(301, 'https://matchbook.academy');
     }
 
     return next();
+}
+
+// HELPERS
+
+function extractHostFromRequest (req) {
+    return req.headers.host;
+}
+
+function extractProtocolFromRequest (req) {
+
+    // NOTE: load balancer adds http header to tell if original request was 'http' or 'https'
+    // Depending on the load balancer it could be lowercased or not.
+
+    if (req.headers['x-forwarded-proto']) {
+        return req.headers['x-forwarded-proto'];
+    }
+
+    if (req.headers['X-Forwarded-Proto']) {
+        return req.headers['X-Forwarded-Proto'];
+    }
+
+    return null;
 }
